1fb342328c55a24b02f640c843f456321dcfd06e
Kishore_kumar.B <kishorekumar.b@bugbusterslabs.com>
2025-01-09 22:51:48 +0530

    q

--- AnimeLib/LibData/views.py
+++ AnimeLib/LibData/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import generics,status
+from rest_framework.response import Response
+from .serializers import AnimeUserserializer
 # Create your views here.
+class UserRegistration(generics.CreateAPIView):
+    serializer_class = AnimeUserserializer
+
+    def create(self, request):
+        serializer = AnimeUserserializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response (serializer.data,status=status.HTTP_201_CREATED)
+
+class UserLogin(generics.GenericAPIView):
+     def post (self, request):
+         user=request.data
