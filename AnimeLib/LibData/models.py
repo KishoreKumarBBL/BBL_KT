7106db9bae28ba23f69d817da4ddccbfb9635b63
Kishore_kumar.B <kishorekumar.b@bugbusterslabs.com>
2025-01-09 22:47:15 +0530

    new

--- AnimeLib/LibData/models.py
+++ AnimeLib/LibData/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.base_user import BaseUserManager,AbstractBaseUser
 # Create your models here.
+class UserData(BaseUserManager):
+    def create_user(self,firstname,lastname,username, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have a username')
+        
+        user = self.model(username=username,email=self.normalize_email(email),firstname=firstname,lastname=lastname)
+        user.set_password(password)
+        user.save()
+        return user
+
+class AnimeUser(AbstractBaseUser):
+    username = models.CharField(max_length=255,unique=True)
+    email = models.EmailField(max_length=255,unique=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+
+objects = UserData()     
--- AnimeLib/LibData/serializers.py
+++ AnimeLib/LibData/serializers.py
@@ -1,2 +1,27 @@
 from rest_framework import serializers
-from.models import UserData
+from.models import AnimeUser
+
+class AnimeUserserializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True, unique=True)
+    email = serializers.EmailField(required=True, unique=True)
+    password = serializers.CharField(required=True)
+    firstname = serializers.CharField(required=True)
+    lastname = serializers.CharField(required=True)
+
+    class Meta:
+        model = AnimeUser
+        fields = ['username','firstname','lastname','email','password']
+
+def validate(self,data):
+    firstname = data.get('firstname','')
+    username = data.get('username','')
+
+    if firstname and firstname.isupper():
+        raise serializers.ValidationError('First name should not be in uppercase')
+    
+    if not username.isalnum():
+        raise serializers.ValidationError('Username should contain alphanumeric only!')
+    return data 
+
+def create(self,validate_data):
+    return AnimeUser.objects.UserData(**validate_data)
