193ac9c51726376b6bfffd38cdb501addbbc4e67
Kishore_kumar.B <kishorekumar.b@bugbusterslabs.com>
2025-01-12 20:22:09 +0530

    P_Z=2

--- MAL/UserData/UserData/customPagination.py
+++ MAL/UserData/UserData/customPagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 class Custompagesettings(PageNumberPagination):
-    page_size = 1
+    page_size = 2
     page_query_param = 'page_limit'
     max_page_size =20
     
