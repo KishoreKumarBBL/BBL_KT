cd5bced5c378aee2617ab3b843af2da9cf12b310
Kishore_kumar.B <kishorekumar.b@bugbusterslabs.com>
2025-01-12 20:19:09 +0530

    Common Pagination added

--- MAL/UserData/Userlist/views.py
+++ MAL/UserData/Userlist/views.py
@@ -4,11 +4,12 @@ from rest_framework import generics,status
 from rest_framework.response import Response
 from .serializers import AnimeUserserializer,Userprofileserializer
 from rest_framework.generics import ListAPIView
-from rest_framework.pagination import PageNumberPagination
 from decouple import config
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from UserData.customPagination import Custompagesettings
+from.pagination import pagestyle
 # from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class Registration(generics.CreateAPIView):# CreateAPIView is used to POST the Data
@@ -20,13 +21,7 @@ class Registration(generics.CreateAPIView):# CreateAPIView is used to POST the D
         serializer.is_valid(raise_exception=True)
         serializer.save() 
         return Response (serializer.data,status=status.HTTP_201_CREATED)
-
-
-class pagestyle(PageNumberPagination):
-    page_size = config("page_size")
-    page_query_param = config("page_param")       
-
-
+    
 
 class UserData(ListAPIView): # ListAPIView is used to GET the Data 
     queryset = AnimeUser.objects.all() # Fetches all the fields from the AnimeUser Model
@@ -48,7 +43,7 @@ class Create_Profile(generics.CreateAPIView):
 class ViewProfile(ListAPIView): # This is used if there is only Superuser without any Authentication
     queryset = UserProfile.objects.all()
     serializer_class = Userprofileserializer
-    pagination_class = pagestyle
+    pagination_class = Custompagesettings
 
     # def get_queryset(self):
     #     return super().get_queryset() This should be used if we need to filter the user details or to fetch the Authenticated user details
@@ -57,7 +52,7 @@ class updateProfile(RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = Userprofileserializer
     lookup_field = 'id'
-    pagination_class = pagestyle
+   
  
     # def update(self, request, *args, **kwargs):
     #     user = self.get_object()
